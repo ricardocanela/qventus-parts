be44d4ea239017e770cb15f22d5b209a9d769cc4
Ricardo Canela <ricardo@roadz.com>
2025-04-23 11:40:31 -0300

    fix: update db hostname

--- src/parts_api/settings/base.py
+++ src/parts_api/settings/base.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -72,26 +73,17 @@ TEMPLATES = [
 WSGI_APPLICATION = "parts_api.wsgi.application"
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR.parent / "data" / "db.sqlite3",
-#     }
-# }
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "partsdb",
-        "USER": "partsuser",
-        "PASSWORD": "partspass",
-        "HOST": "db",
-        "PORT": "5432",
+        "NAME": os.getenv("POSTGRES_DB", "partsdb"),
+        "USER": os.getenv("POSTGRES_USER", "partsuser"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "partspass"),
+        "HOST": os.getenv("POSTGRES_HOST", "localhost"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
